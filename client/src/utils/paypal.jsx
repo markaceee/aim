2c4d842f6695851664e03756fdea5a389b124ebd
Mark Ace Rosete Boquiren <mboquiren@tspi.com.ph>
2023-12-22 08:09:33 +0800

    Fixed url

--- client/src/utils/paypal.jsx
+++ client/src/utils/paypal.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/api/paypal",
+    // baseURL: "http://localhost:8080/api/paypal",
+    baseURL: "https://aim-lgn2.onrender.com/api/aim",
 });
 
 export default instance;
