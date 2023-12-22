82f5b6c224f308b256d2638ae173e2e88cb7d620
MARLON_LANDER ORTEGA <teddyboss0918@gmail.com>
2023-12-22 16:05:24 +0800

    test

--- server/src/main/java/aim/server/config/OpenAIConfig.java
+++ server/src/main/java/aim/server/config/OpenAIConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class OpenAIConfig {
 
-    @Value("${openai.api.key}")
+    @Value("${AIM_SECRET_KEY}")
     String openaiApiKey;
 
     @Bean
