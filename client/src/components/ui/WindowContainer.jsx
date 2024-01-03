795bb49fb0e9e5c49291c6c82ec56b45becdf2c9
Mark Ace Boquiren <mboquiren@tspi.com.ph>
2024-01-04 07:21:36 +0800

    Fixed header 2

--- client/src/components/ui/WindowContainer.jsx
+++ client/src/components/ui/WindowContainer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const WindowContainer = ({ headerTitle, children, header}) => {
   return (
     <div className='relative'>
-      <div className={`bg-[${ header ? "#252C41" : "#515A79"}] text-white p-3`}>{headerTitle}</div>
+      <div className={`${ header ? "dark-header" : "white-header"} text-white p-3`}>{headerTitle}</div>
       <div className="border border-solid">{children}</div>
     </div>
   );
