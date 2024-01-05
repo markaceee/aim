10da88f6bcdc7943c476f28fe6bfcc1c64b1777f
Mark Ace Boquiren <mboquiren@tspi.com.ph>
2024-01-05 10:58:28 +0800

    Fixed UI

--- client/src/pages/Instructor/Instructor.jsx
+++ client/src/pages/Instructor/Instructor.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Instructor = () => {
+    // const toast = useToast()
     return (
         <div>
             <Link to={"/instructor/register"}>Apply now</Link>
+            {/* {toast({
+                title: `top-right toast`,
+                position: 'top-right',
+                isClosable: true,
+            })} */}
         </div>
     )
 }
